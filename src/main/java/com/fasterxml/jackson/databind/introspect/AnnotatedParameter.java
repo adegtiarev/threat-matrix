f88a1d3824945235703bd86376bcbed32ae0727a
Andrei Degtiarev <adegtiarev@gmail.com>
2023-06-12 16:30:17 -0300

    update

--- src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedParameter.java
+++ src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedParameter.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/FasterXML/jackson-databind/releases/tag/jackson-databind-2.0.2
+*    Source File: AnnotatedParameter.java
+*    
+*    Copyrights:
+*      copyright (c) 2007- tatu saloranta, tatu.saloranta@iki.fi
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.fasterxml.jackson.databind.introspect;
 
 import java.lang.annotation.Annotation;
