9e722511bba4ca17d78d03b164216458847f8ce3
Andrei Degtiarev <adegtiarev@gmail.com>
2023-11-13 21:58:16 -0300

    update

--- src/main/java/com/fasterxml/jackson/databind/jsontype/package-info.java
+++ src/main/java/com/fasterxml/jackson/databind/jsontype/package-info.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/FasterXML/jackson-databind/releases/tag/jackson-databind-2.6.7.5
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright (c) 2007- tatu saloranta, tatu.saloranta@iki.fi
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Package that contains interfaces that define how to implement
  * functionality for dynamically resolving type during deserialization.
